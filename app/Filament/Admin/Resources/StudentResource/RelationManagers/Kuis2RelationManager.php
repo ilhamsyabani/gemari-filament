763811e7aefd7c4eed0b01c692862ee5830b0aac
ilham sya <ilsya@ilhams-MacBook-Air.local>
2024-01-01 10:02:06 +0700

    first commit

---
+++ app/Filament/Admin/Resources/StudentResource/RelationManagers/Kuis2RelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Admin\Resources\StudentResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class Kuis2RelationManager extends RelationManager
+{
+    protected static string $relationship = 'kuis2';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nilai')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('nilai')
+            ->columns([
+                Tables\Columns\TextColumn::make('nilai'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
---
+++ app/Filament/Admin/Resources/StudentResource/RelationManagers/Kuis4RelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Admin\Resources\StudentResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class Kuis4RelationManager extends RelationManager
+{
+    protected static string $relationship = 'kuis4';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('kalimat')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('kalimat')
+            ->columns([
+                Tables\Columns\TextColumn::make('kalimat'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
